a31676c283bb47f13a9e93fcc94449556d7f7696
chetanshankar <chetan.shankar@gmail.com>
2025-11-16 10:19:08 -0800

    Web server

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'hi there')
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 5370), MyHandler)
+    print('Server running on port 5370...')
+    server.serve_forever()
+    
