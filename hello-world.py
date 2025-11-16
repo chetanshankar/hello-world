34a4c6b999e3d35b2b68769a5f1a9cc7dfbc70a2
chetanshankar <chetan.shankar@gmail.com>
2025-11-16 10:56:23 -0800

    Fixed bug in web server

--- hello-world.py
+++ hello-world.py
@@ -2,10 +2,14 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b'hi there')
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello world")
+        else:
+            self.send_error(404, "Not Found")
+
 
 if __name__ == '__main__':
     server = HTTPServer(('0.0.0.0', 5370), MyHandler)
